cae527f44a146432c5e4d7480ad91bad6120f987
danhoui <danielhouli@yahoo.com>
2016-04-17 18:17:27 -0400

    version 4.2

---
+++ app/src/main/java/com/cs683/danhoui/myclassproject/Balloons.java
@@ -0,0 +1,36 @@
+package com.cs683.danhoui.myclassproject;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//image used from google, orginal source myugavetschoolexperience.wordpress.com
+
+
+public class Balloons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balloons);
+
+
+        //set timer in milli to display the splash screen = 3 seconds
+        int splashTimer = 10000;
+
+        //create handler to start next activity
+        Handler myHandler = new Handler();
+        //using postDelayed to allow it to be run using the 3 seconds timer
+        myHandler.postDelayed(new Runnable() {
+            public void run() {
+                //We run to start next activity
+                //send us to the Main Activity class
+                Intent myIntent = new Intent(Balloons.this, RewardWon.class);
+                //move us to the MainActivity class
+                Balloons.this.startActivity(myIntent);
+                Balloons.this.finish();
+            }
+        }, splashTimer);//our timer variable is here
+    }
+}
--- app/src/main/java/com/cs683/danhoui/myclassproject/RewardDBReader.java
+++ app/src/main/java/com/cs683/danhoui/myclassproject/RewardDBReader.java
@@ -71,7 +71,7 @@ public class RewardDBReader extends AppCompatActivity {
                     public void run() {
                         //We run to start next activity
                         //send us to the Main Activity class
-                        Intent myIntent = new Intent(RewardDBReader.this, RewardWon.class);
+                        Intent myIntent = new Intent(RewardDBReader.this, Balloons.class);
                         //move us to the MainActivity class
                         RewardDBReader.this.startActivity(myIntent);
                         RewardDBReader.this.finish();
